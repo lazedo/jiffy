cf70573e1c885a92d1ee30735e623eadb6dde2d2
lazedo <luis.azedo@factorlusitano.com>
2018-10-05 02:09:25 +0100

    include char as-is if its not utf8

--- c_src/decoder.c
+++ c_src/decoder.c
@@ -281,9 +281,10 @@ dec_string(Decoder* d, ERL_NIF_TERM* value)
         } else {
             ulen = utf8_validate(&(d->u[d->i]), d->len - d->i);
             if(ulen < 0) {
-                return 0;
+                d->i++;
+            } else {
+                d->i += ulen;
             }
-            d->i += ulen;
         }
     }
 
